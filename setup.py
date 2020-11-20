9a21e621edb70c22f444f98fd1b4efa6f81c99d9
Jessy Barrette <jessy.barrette@hakai.org>
2020-11-20 13:22:08 -0800

    add include_package_data=True in setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from setuptools import setup
 
 setup(name='hakai_qc',
       version='0.0.1',
-      description='Hakai methods used to config datasets',
+      description='Hakai methods used to test qc config a datasets',
       url='https://github.com/tayden/hakai-profile-qaqc',
       author='Jessy Barrette',
       author_email='jessy.barrette@hakai.org',
       license='MIT',
       packages=['hakai_qc'],
+      include_package_data=True,
       install_requires=[
           'future',
           'requests',
