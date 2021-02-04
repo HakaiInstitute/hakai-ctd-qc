e74de2951207448abc79928d68a0c529fb0a5212
Jessy Barrette <jessy.barrette@hakai.org>
2021-02-03 16:57:13 -0800

    Add a hakai_id with a some grey list flags to review

--- test/test_hakai_database_update_tool.py
+++ test/test_hakai_database_update_tool.py
@@ -2,7 +2,9 @@ import hakai_qc
 from hakai_api import Client
 
 # Use hakai_id list method
-hakai_id_list = ['080217_2017-01-08T18:03:05.167Z', '01907674_2016-10-12T16:23:36Z']
+hakai_id_list = ['080217_2017-01-08T18:03:05.167Z', '01907674_2016-10-12T16:23:36Z', '080217_2015-09-10T23:07:01.167Z']
+# 080217_2015-09-10T23:07:01.167Z has a broken do and fluo sensor test for grey list
+
 json_output_from_hakai_id_method = hakai_qc.run.update_hakai_ctd_profile_data(hakai_id=hakai_id_list)
 
 # Use JSON input method
