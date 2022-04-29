9fb6f912ce7f71d09b4e7434cc43d654a9c9b553
Jessy Barrette <jessy.barrette@hakai.org>
2022-04-29 09:51:44 -0400

    Improve provisional dataset generation

--- script/generate_provisional_files.py
+++ script/generate_provisional_files.py
@@ -7,11 +7,25 @@ import argparse
 import json
 from tqdm import tqdm
 import re
+import os
 
 def rename_columns(col):
+    if col in ['direction_flag']:
+        return col
     col = re.sub('_flag$','_flag_description',col)
-    col = re.sub('_flag_level_1$','_UQL',col)
+    col = re.sub('_flag_level_1$','_qartod_flag',col)
     return col
+
+CHUNK_SIZE=300
+parser = argparse.ArgumentParser(description="QC Hakai CTD Profiles")
+parser.add_argument(
+    "--output_path",
+    type=str,
+    help="Which database is run the script",
+    default="./output",
+)
+args = parser.parse_args()
+output_path = args.output_path
 
 # Connect to API
 client = Client()
@@ -20,16 +34,30 @@ ctd_cast_endpoint = "/ctd/views/file/cast"
 ctd_cast_data_endpoint = "/ctd/views/file/cast/data"
 
 # Get Cast Data
-processed_cast_filter = "processing_stage={8_rbr_processed,8_binAvg}&station_latitude!=null&station_longitude!=null&limit=1000&fields=station&distinct"
+processed_cast_filter = "processing_stage={8_rbr_processed,8_binAvg,9_qc_auto,10_qc_pi}&status==null&station_latitude!=null&station_longitude!=null&work_area={CALVERT,QUADRA,JOHNSTONE STRAIT}&limit=-1&fields=work_area,station,hakai_id"
 url = f"{client.api_root}{ctd_cast_endpoint}?{processed_cast_filter}"
 response = client.get(url)
-stations = pd.DataFrame(response.json())['station'].tolist()
+df_stations = pd.DataFrame(response.json())
+chunks = round(len(df_stations)/CHUNK_SIZE)
+for chunk in np.array_split(df_stations, chunks):
+    # Run QC tests
+    df = hakai_profile_qc.review.run_tests(hakai_id=chunk['hakai_id'].to_list())
+    
+    # Replace flag values from seabird
+    df = df.replace({-9.99E-29: np.nan})
 
-for station in stations:
-    df = hakai_profile_qc.review.run_tests(station=[station])
-    # rename columns 
+    # Consider only downcast data
+    df['direction_flag'] = df['direction_flag'].fillna('d')
+    df = df.query("direction_flag=='d'") # Ignore up cast and static data in provisional dataset
+
+    # Rename Columns 
     df.columns = [rename_columns(col) for col in df.columns]
     df = df.drop(columns=['weather','bin_stats'])
-    df.to_xarray().to_netcdf(f"{station}.nc")
 
-
+    # Save data grouped xarray datasets just to serve temporarily ERDDAP
+    for (work_area,station,hakai_id),df_profile in tqdm(df.groupby(['work_area','station','hakai_id']),desc='Save each individual files:',unit='file'):
+        dir_path = os.path.join(output_path,work_area,station)
+        if not os.path.isdir(dir_path):
+            os.makedirs(dir_path)
+        file_output = os.path.join(dir_path, f"{hakai_id}.nc")
+        df_profile.to_xarray().to_netcdf(file_output)
