92a77792d879beb7dece11e7de72f687aea64b8d
Jessy Barrette <30420025+JessyBarrette@users.noreply.github.com>
2024-08-12 12:50:49 -0400

    isort black

--- hakai_profile_qc/version.py
+++ hakai_profile_qc/version.py
@@ -1,9 +1,11 @@
-from pathlib import Path
 import tomllib
+from pathlib import Path
+
 
 def get_version_from_pyproject():
     pyproject = Path(__file__).parent.parent / "pyproject.toml"
     data = tomllib.loads(pyproject.read_text())
     return data["tool"]["poetry"]["version"]
+
 
 __version__ = get_version_from_pyproject()
