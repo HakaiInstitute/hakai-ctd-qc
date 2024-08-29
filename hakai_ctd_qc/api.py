5285935f4ac3cbbe46b0b14601a1799ec1c97e51
Jessy Barrette <30420025+JessyBarrette@users.noreply.github.com>
2024-08-29 13:01:02 -0400

    add manual qc status page

--- hakai_ctd_qc/api.py
+++ hakai_ctd_qc/api.py
@@ -1,5 +1,6 @@
 import os
 from contextlib import asynccontextmanager
+from pathlib import Path
 
 import fastapi
 import pandas as pd
@@ -10,9 +11,12 @@ from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.triggers.cron import CronTrigger
 from cron_descriptor import get_description
 from fastapi import Depends, Header, HTTPException
+from fastapi.responses import HTMLResponse
 from loguru import logger
+from hakai_api import Client
 
 from hakai_ctd_qc.__main__ import main as qc_profiles
+import panel as pn
 
 
 def get_version_from_pyproject():
@@ -25,7 +29,7 @@ version = get_version_from_pyproject()
 
 API_ROOT = os.getenv("HAKAI_API_ROOT", "https://goose.hakai.org/api")
 HOST = os.getenv("HOST", "127.0.0.1")
-PORT = os.getenv("PORT", 8000)
+PORT = int(os.getenv("PORT", 8000))
 DEBUG = os.getenv("DEBUG", False)
 TOKENS = os.getenv("TOKENS", "").split(",")
 QC_CRON = os.getenv("QC_CRON")
@@ -187,5 +191,71 @@ async def run_quality_control_on_hakai_profiles(
     return {"id": id, "status": "scheduled"}
 
 
+@app.get("/manual-qc-status")
+async def get_manual_qced():
+    client = Client(credentials=os.getenv("HAKAI_API_TOKEN"))
+    logger.info("Getting manual QCed data")
+    response = client.get(API_ROOT + "/eims/views/output/ctd_qc?limit=-1")
+    response.raise_for_status()
+    df_qc = pd.DataFrame(response.json())
+
+    logger.info("Get cast data")
+    response = client.get(API_ROOT + "/ctd/views/file/cast?limit=-1&fields=organization,work_area,station,hakai_id,start_dt")
+    response.raise_for_status()
+    df_cast = pd.DataFrame(response.json())
+
+    # combine the two dataframes
+    df = pd.merge(df_qc, df_cast, on=["work_area","hakai_id"], how="outer")
+    flag_columns = df.filter(like="_flag").columns
+    summary = []
+    for index,df_group in df.groupby(['organization', 'work_area', 'station']):
+
+        qced = df_group.dropna(how='all', subset=flag_columns)
+        if qced.empty:
+            continue
+
+        summary.append({
+            "organization": index[0],
+            "work_area": index[1],
+            "station": index[2],
+            "n_drops": len(df_group),
+            "n_qced": len(qced),
+            **qced[flag_columns].count().to_dict(),
+            "last_drop_qced": qced['start_dt'].max()
+        })
+
+    df_summary = pd.DataFrame(summary)
+    html_table = df_summary.to_html(index=False, classes='display', table_id='dataTable')
+
+    # Embed DataTables JavaScript and CSS
+    html_string = f"""
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Manual QCed Data</title>
+        <link rel="stylesheet" href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css">
+        <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+        <script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
+        <script>
+            $(document).ready(function() {{
+                $('#dataTable').DataTable();
+            }});
+        </script>
+    </head>
+    <body>
+        <h1>Hakai CTD Profiles Manually QCed Status</h1>
+        <p>Number of drops and number of drops that have been manually QCed</p>
+        {html_table}
+    </body>
+    </html>
+    """
+
+    return HTMLResponse(content=html_string, status_code=200)
+
+    
+
 if __name__ == "__main__":
     uvicorn.run(app, host=HOST, port=PORT)
+
